27442f53bf2982689fbcd767ee130376f01d7782
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-04 11:53:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  BRAND = []
+  
 
   def initialize(brand)
     @brand = brand
@@ -11,8 +11,6 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-    def brand=(brand)
-    @brand = brand
-    BRAND << brand 
+
   end
 end
