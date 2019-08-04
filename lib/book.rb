4098e67b1a532584093c0a56e51460c0996e2e42
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-04 11:42:27 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,17 @@
+class Book
+  attr_accessor :author, :page_count  # remove the attr_accessor for genre
+  attr_reader :title, :genre  # add an attr_reader for genre
+ 
+  GENRES = []
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
 # class Book
 #   attr_accessor :author, :page_count, :genre
 #   attr_reader :title
